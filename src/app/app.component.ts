b4a0217d330e56a1755777f9c8ff942d233ad461
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-11-17 14:20:25 +0300

    Modify parent compnent to add list of foods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Meal Tracker';
-  // public foods: Food[] = [
-  //   new Food
-  // ]
+  public foods: Food[] = [
+    new Food("Hamburger", "Didn't get a soda or cheese on my burger", 354),
+    new Food("Fries", "I only ate half of them",365)
+  ];
+}
+export class Food {
+  public done: boolean = false;
+  constructor(public name: string, public details: string, public calories: number) {}
 }
